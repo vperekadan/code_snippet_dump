9cf2ff5e8ec409000939b8a48015efa9ec7fce5f
vperekadan <vp0023@uah.edu>
2023-12-03 20:17:56 -0600

    georef_array

--- georef_data.py
+++ georef_data.py
@@ -2,6 +2,14 @@ import rasterio as rio
 from numpy import ndarray
 
 
-def georeference_ndarry(data_array: ndarray, src_file_path: str, output_filename: str) -> None:
-    with rio.open(src_file_path) as src:
-        src_meta = src.
+def georeference_ndarry(
+    data_array: ndarray, src_file_path: str, output_filename: str
+) -> None:
+    with rio.open(src_file_path, "r") as src, rio.open(
+        output_filename, "w", **src.profile
+    ) as dst:
+        src_meta = src.profile
+        assert (
+            src_meta["width"] == 0 and src_meta["height"] == 1
+        ), "Dimensions of the array and src tif doesn't match..."
+        dst.write(data_array)
